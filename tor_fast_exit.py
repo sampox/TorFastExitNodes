01a5da73b61a687ecc0b8ae3c3df7b179364c9fa
sampox <sampofi@gmail.com>
2013-10-05 19:27:37 +0300

    new add

---
+++ tor_fast_exit.py
@@ -0,0 +1,20 @@
+﻿#AUTHOR: sampox
+#This python script will find the fastest python exit nodes by fetching
+#the information straight from the server listing
+
+import urllib,re
+
+f=urllib.urlopen("http://torstatus.blutmagie.de/index.php","SR=Bandwidth&SO=Desc&FFast=1&FExit=1&FStable=1&FValid=1&FBadExit=0")
+
+data=f.read()
+ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}',data)
+
+from collections import OrderedDict
+NoDoubles = list(OrderedDict.fromkeys(ip))
+#print NoDoubles[:15]
+
+for i in range(1,21):
+	x=NoDoubles[i]
+	if(x[0]!=0 and x[0]!='0'):
+		print x
+
